85a9990e68c4d021f9797c74a30dcf02abc48e59
zrfan@leqee.com <zrfan@leqee.com>
2020-01-29 14:16:53 +0800

    leetcode 136

---
+++ src/main/java/tag/链表/easy/LeetCode_136.java
@@ -0,0 +1,33 @@
+package tag.链表.easy;
+
+/**
+ * @author zrfan
+ * @date 2020/01/29.
+ */
+public class LeetCode_136 {
+
+  /**
+   * 二叉树反转
+   *
+   * 解题思路
+   *  递归
+   *  left=right
+   *  right=left
+   *  return root;
+   */
+
+//  public TreeNode invertTree(TreeNode root) {
+//
+//    if(root == null){
+//      return null;
+//    }
+//
+//    TreeNode left = invertTree(root.right);
+//    TreeNode right = invertTree(root.left);
+//
+//    root.left = left;
+//    root.right = right;
+//
+//    return root;
+//  }
+}
