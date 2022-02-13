756dd00dfb740af591230ab90cbf4d3497160dcf
zrfan <13588034686@163.com>
2022-02-13 14:02:37 +0800

    feat: leetcode_92
    1.反转链表II
    2.找好待反转的及前后两段

--- src/main/java/common/ListNode.java
+++ src/main/java/common/ListNode.java
@@ -11,11 +11,14 @@ public class ListNode {
   public int val;
   public ListNode next;
 
-  ListNode(int x) {
-    val = x;
+  public ListNode() {
   }
 
-  ListNode(int val, ListNode next) {
+  public ListNode(int x) {
+    this.val = x;
+  }
+
+  public ListNode(int val, ListNode next) {
     this.val = val;
     this.next = next;
   }
---
+++ src/main/java/tag/链表/medium/ReverseLinkedListII_92.java
@@ -0,0 +1,63 @@
+package tag.链表.medium;
+
+import common.ListNode;
+
+/**
+ * Input: head = [1,2,3,4,5], left = 2, right = 4
+ * Output: [1,4,3,2,5]
+ *
+ * @author zrfan
+ * @date 2022/02/13.
+ */
+public class ReverseLinkedListII_92 {
+
+  /**
+   * 反转链表的一部分
+   * 思路
+   * 1.将待反转的链表分成3段
+   *
+   * @param head  头节点
+   * @param left  起始
+   * @param right 结束
+   * @return 反转后的链表
+   */
+  public ListNode reverseBetween(ListNode head, int left, int right) {
+
+
+    // 1 -> 2 -> 3 -> 4 -> 5  ===> 1 -> 4 -> 3 -> 2 -> 5
+    ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+
+    // 找到 第一段的最后一个节点, 即1
+    ListNode first = dummy;
+    for (int i = 1; i < left; i++) {
+      first = first.next;
+    }
+
+    // 反转前第二段的第一个节点，即2，反转后变成第二段的最后一个节点
+    ListNode second = first.next;
+
+    if (second == null) {
+      return dummy.next;
+    }
+
+    ListNode l = second;
+    ListNode r = l.next;
+
+    for (int i = left; i < right; i++) {
+
+      ListNode temp = r.next;
+      r.next = l;
+      l=r;
+      r=temp;
+
+    }
+
+    //上面循环结束，中间这段就反转完了 变成 1 -> 2<-3<-4 -> 5, 需要将1指向4，2指向5
+
+    first.next = l;
+    second.next = r;
+
+    return dummy.next;
+  }
+}
