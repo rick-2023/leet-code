7f97ac8209aa2fb7cf78353deb72a4b8ec9bdde2
zrfan <13588034686@163.com>
2022-02-13 17:47:34 +0800

    feat: leetcode_83
    1.快慢指针

--- src/main/java/tag/linkedList/easy/RemoveDuplicatesFromSortedList_83.java
+++ src/main/java/tag/linkedList/easy/RemoveDuplicatesFromSortedList_83.java
@@ -1,5 +1,7 @@
 package tag.linkedList.easy;
 
+import common.ListNode;
+
 /**
  * @author zrfan
  * @date 2019/12/21.
@@ -8,10 +10,28 @@ public class RemoveDuplicatesFromSortedList_83 {
 
   /**
    * 解题思路
-   *  定义一个当前节点指向head
-   *  while 循环
-   *    if 当前节点的值与next值一样，则当前节点指向next.next
-   *    else 指向next
+   * 定义一个当前节点指向head
+   * while 循环
+   * if 当前节点的值与next值一样，则当前节点指向next.next
+   * else 指向next
    * return head
    */
+
+  public ListNode deleteDuplicates(ListNode head) {
+
+    ListNode cur = head;
+
+    while (cur!=null && cur.next != null){
+
+      if (cur.val == cur.next.val) {
+        cur.next = cur.next.next;
+      } else {
+        cur = cur.next;
+      }
+
+    }
+
+    return head;
+
+  }
 }
