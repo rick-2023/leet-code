ba8d8bcfed3f0891333e7cded3a92b776b55185d
zrfan <13588034686@163.com>
2022-10-16 21:47:22 +0800

    feat:leetcode_5
    1.左右指针

--- src/main/java/tag/DP/medium/LongestPalindromicSubstring_5.java
+++ src/main/java/tag/DP/medium/LongestPalindromicSubstring_5.java
@@ -1,12 +1,17 @@
 package tag.DP.medium;
 
+import org.junit.Test;
+
 /**
  * 给定字符串，返回最长的回文子串
- *
+ * <p>
  * 首先考虑用动态规划 三步走
- *  1.状态容器定义，一般是一维或二维数组
- *  2.初始化 比如本题对角线都置为 true
- *  3.建立状态转移方程，本地为 dp[i][j] = dp[i+1][j-1]; 取决于最里面的
+ * 1.状态容器定义，一般是一维或二维数组
+ * 2.初始化 比如本题对角线都置为 true
+ * 3.建立状态转移方程，本地为 dp[i][j] = dp[i+1][j-1]; 取决于最里面的
+ *
+ *
+ * 解法二 左右指针
  *
  * @author zrfan
  * @date 2021/07/06.
@@ -65,4 +70,42 @@ public class LongestPalindromicSubstring_5 {
   }
 
 
+  // 从中间到两端扩散的双指针
+  public static String longestPalindrome2(String s) {
+
+    String res = "";
+    for (int i = 0; i < s.length(); i++) {
+
+      String s1 = help(s, i, i);
+      String s2 = help(s, i, i + 1);
+
+      res = res.length() > s1.length() ? res : s1;
+      res = res.length() > s2.length() ? res : s2;
+
+    }
+
+    return res;
+
+  }
+
+  public static String help(String s, int l, int r) {
+
+    while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
+      l--;
+      r++;
+    }
+
+    return s.substring(l + 1, r);
+
+  }
+
+
+  @Test
+  public void test(){
+
+    String res = longestPalindrome2("abbc");
+    System.out.println(res);
+
+
+  }
 }
