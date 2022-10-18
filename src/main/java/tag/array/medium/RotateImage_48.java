48f4865f2671dfa1bbccbb54bd0078ebd5a31c8c
zrfan <13588034686@163.com>
2022-10-18 22:24:03 +0800

    feat:leetcode_48
    1.旋转数组

--- src/main/java/tag/array/medium/RotateImage_48.java
+++ src/main/java/tag/array/medium/RotateImage_48.java
@@ -1,13 +1,14 @@
 package tag.array.medium;
 
+import org.junit.Test;
+
 /**
- *
  * Input: matrix = [
  * [1,2,3],
  * [4,5,6],
  * [7,8,9]]
- *
- *
+ * <p>
+ * <p>
  * Output: [
  * [7,4,1],
  * [8,5,2],
@@ -18,10 +19,63 @@ package tag.array.medium;
  */
 public class RotateImage_48 {
 
-  public void rotate(int[][] matrix) {
+  public static void rotate(int[][] matrix) {
+
+    // 沿左上-右下 对称线 旋转
+    int n = matrix.length;
+
+    for (int i = 0; i < n; i++) {
+      for (int j = i; j < n; j++) {
+
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+
+      }
+
+    }
+
+    // 反转每一行
+    for (int[] each : matrix) {
+      help(each);
+    }
 
 
+    // 输出matrix
+    for (int i = 0; i < matrix.length; i++) {
+      for (int j = 0; j < matrix[0].length; j++) {
+        System.out.print(matrix[i][j] + " ");
+      }
 
+      System.out.println();
+
+    }
+
+
+  }
+
+  private static void help(int[] each) {
+
+    int i = 0, j = each.length - 1;
+
+    while (i < j) {
+
+      int temp = each[i];
+      each[i] = each[j];
+      each[j] = temp;
+
+      i++;
+      j--;
+
+
+    }
+
+
+  }
+
+  @Test
+  public void test(){
+    rotate(new int[][]{{1,2,3},{4,5,6}, {7,8,9}});
   }
 
 }
