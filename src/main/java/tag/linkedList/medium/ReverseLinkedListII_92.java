6f7ad1d15b6963a8b3dbe3c50366defd0ead7944
zrfan <13588034686@163.com>
2023-05-14 10:25:32 +0800

    feat:leetcode_92
    1.新增解法

--- src/main/java/tag/linkedList/medium/ReverseLinkedListII_92.java
+++ src/main/java/tag/linkedList/medium/ReverseLinkedListII_92.java
@@ -48,8 +48,8 @@ public class ReverseLinkedListII_92 {
 
       ListNode temp = r.next;
       r.next = l;
-      l=r;
-      r=temp;
+      l = r;
+      r = temp;
 
     }
 
@@ -59,5 +59,55 @@ public class ReverseLinkedListII_92 {
     second.next = r;
 
     return dummy.next;
+  }
+
+
+  /**
+   * 思路2 把left 和 right之间的 反转重新插入, 先把3 移到1-2之间，再把4移到 1-3之间
+   * <p>
+   * Input: head = [1,2,3,4,5], left = 2, right = 4
+   * Output: [1,4,3,2,5]
+   *
+   * @param head  头节点
+   * @param left  起始
+   * @param right 结束
+   * @return 反转后的链表
+   */
+    public ListNode reverseBetween2(ListNode head, int left, int right) {
+
+    if (head == null) return null;
+
+    ListNode dummy = new ListNode(-1);
+    dummy.next = head;
+
+    // 反转前的最后一个节点  1
+    ListNode p = dummy;
+    for (int i = 0; i < left - 1; i++) {
+      p = p.next;
+    }
+
+    int count = right - left;
+    // 反转的第i+1个 即2
+    ListNode cur = p.next;
+
+    for (int i = 0; i < count; i++) {
+
+      // 待插入的节点 3
+      ListNode insertNode = cur.next;
+      // 暂存一下 2
+      ListNode temp = p.next;
+      // 把2的下一个指针 指向 4
+      cur.next = cur.next.next;
+
+      // 1指向3
+      p.next = insertNode;
+      // 3指向 2，依次循环往复
+      insertNode.next = temp;
+
+
+    }
+
+    return dummy.next;
+
   }
 }
